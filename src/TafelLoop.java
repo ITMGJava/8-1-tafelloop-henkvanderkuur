f7f9e0b08537bcde0a37a79145350b30d8e70f36
Henk van der Kuur <info@vanderkuur.com>
2020-09-29 10:53:24 +0200

    Tafel loop, Nog een keer? v1.0

--- src/TafelLoop.java
+++ src/TafelLoop.java
@@ -1,2 +1,31 @@
+import java.util.Scanner;
+
 public class TafelLoop {
+
+    public static void main(String[] args) {
+        var eenkeer = 1;
+        while (eenkeer > 0) {
+            System.out.println();
+            System.out.print("Welk getal wil je vermenigvuldigen met 1 tot en met 10? ");
+            var s = new Scanner(System.in);
+            var tafelgetal = s.nextInt();
+
+            for (int i = 1; i <= 10; i++) {
+                System.out.println(i + " * " + tafelgetal + " = " + i * tafelgetal);
+            }
+            System.out.println();
+            System.out.print("NOG EEN KEER? (ja of nee):  ");
+            var n = new Scanner(System.in);
+            var nogeenkeer = n.nextLine();
+            nogeenkeer = nogeenkeer.toLowerCase();
+            if (nogeenkeer.equals("nee")) {
+                System.out.println();
+                System.out.println("BEDANKT EN TOT ZIENS!");
+                System.out.println();
+                System.out.println();
+                eenkeer = eenkeer - 1;
+            }
+        }
+    }
 }
+
